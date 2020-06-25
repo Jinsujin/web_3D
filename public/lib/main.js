1486db9ca5655e0de9db00f3c403034427b794ea
Jinsujin <sylpid003@gmail.com>
2020-06-25 11:54:29 +0900

    gitignore 설정, npm 초기화 바벨설치, three js 큐브만들기

---
+++ public/lib/main.js
@@ -0,0 +1,1 @@
+"use strict";function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}var Circle=function a(b){_classCallCheck(this,a),this.color=b};
